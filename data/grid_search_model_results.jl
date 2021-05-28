3107b798f5b7eb40c8b66b5d276f68c3a3fa5edd
Nathan <sandersn@gmail.com>
2021-05-28 10:28:51 -0400

    Submitted version

--- data/grid_search_model_results.jl
+++ data/grid_search_model_results.jl
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5dc907a36595687e7a591266cb3faa0b419b4546445b8d71a32db8b11bb306c3
-size 10438630
+oid sha256:3b09b4d263bf8dd1659bcb5f47923d84dfaeb34cb0dbf2ea72b7cbb07ffd6f52
+size 10507270
--- data/pyhawkes_models.jl
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:9ca0a5a6904618bb8711bda63993241a891a59df65e2dc68a550f04a01a7eec3
-size 18416689
